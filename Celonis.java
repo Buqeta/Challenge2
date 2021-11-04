c4bd054cb7ed99233b71c3567c4e480c6f482c71
Buqete Mucolli <buqetemucolli@gmail.com>
2021-11-04 11:54:53 +0100

    Test

--- Celonis.java
+++ Celonis.java
@@ -1,12 +1,4 @@
 public class Celonis {
-	
-	public void methodChanged() {
-		System.out.println("method1");	
-	}
-	
-	public void method2() {
-		System.out.println("method2");
-	}
 
 	public void method3() {
 		System.out.println("method3");	
